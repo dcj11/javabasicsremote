73267c1fa076d848596a4eea290a41adf1d73175
Robert <bobgillie134@gmail.com>
2022-09-25 15:31:57 +0530

    Initial commit

---
+++ src/learning/helloworld.java
@@ -0,0 +1,11 @@
+package learning;
+
+import com.sun.source.doctree.SystemPropertyTree;
+
+public class helloworld {
+    public static void main(String[] args) {
+
+        String H = "Hello World";
+        System.out.println(H);
+    }
+}
